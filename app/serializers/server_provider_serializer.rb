f8ac8d0a8b0f21b431db12922f577a76269d523d
AndriiMysko <usandrewmisko@gmail.com>
2021-09-15 15:35:31 +0300

    Fix serializers

--- app/serializers/server_provider_serializer.rb
+++ app/serializers/server_provider_serializer.rb
@@ -15,5 +15,5 @@ class ServerProviderSerializer < ApplicationSerializer
 
   attribute(:type) { |server| PROVIDER_KLASS[server.type.constantize] }
   attribute(:username) { |server| server.settings(:p4_host).username }
-  attribute(:permission) { |server, _params| PERMISSION[server.server_provider_permissions&.first&.permission] }
+  attribute(:permission) { |server, params| PERMISSION[params[:current_user].server_provider_permission(server.id)&.permission] }
 end
