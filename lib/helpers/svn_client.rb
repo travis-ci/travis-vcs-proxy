018dee121441ae8a026807b0e633594ecd06103e
AndriiMysko <usandrewmisko@gmail.com>
2021-10-06 15:39:14 +0300

    svn_client: assembla svn handling

--- lib/helpers/svn_client.rb
+++ lib/helpers/svn_client.rb
@@ -1,31 +1,38 @@
 # frozen_string_literal: true
 
+require 'uri'
+require 'tempfile'
+
 module Travis
   module VcsProxy
     class SvnClient
-      attr_accessor :username, :ssh_key, :password
+      attr_accessor :username, :ssh_key, :password, :url
 
-      def exec(cmd)
+      def exec(repo, cmd)
         return `svn --username #{@username} --password #{@password} #{cmd}` if @password
 
-        ssh_file = Tempfile.new('sshkey')
+        ssh_file = ::Tempfile.new('sshkey')
         ssh_file.write(@ssh_key)
-        ENV['SVN_SSH'] = sshfile.path
+        ENV['SVN_SSH'] = "ssh -i #{ssh_file.path}"
+        if assembla?
+          ENV['SVN_SSH'] = "ssh -i #{ssh_file.path} -o SendEnv=REPO_NAME -l svn"
+        end
         ssh_file.close
+        ENV['REPO_NAME'] = repo
         `svn #{cmd}`
       ensure
         ssh_file&.unlink
       end
 
       def ls(repo, branch = nil)
-        res = exec("ls #{url}/#{repo}/#{get_branch(branch)}")
+        res = exec(repo, "ls #{repository_path(repo)}/#{get_branch(branch)}")
         return [] unless res
 
         res.split("\n")
       end
 
       def branches(repo)
-        res = exec("ls #{url}/#{repo}/branches")
+        res = exec(repo, "ls #{repository_path(repo)}/branches")
         return [] unless res
 
         res.split("\n")
@@ -33,22 +40,38 @@ module Travis
 
       def content(repo, file, branch: nil, revision: nil)
         params = "-r #{revision}" if revision
-        exec("cat #{url}/#{repo}/#{get_branch(branch)}/#{file} #{params}")
+        exec(repo, "cat #{repository_path(repo)}/#{get_branch(branch)}/#{file} #{params}")
       end
 
       def log(repo, file, branch: nil, revision: nil, format: nil)
         params = ''
         params += '--xml' if format
         params += "-r #{revision}" if revision
-        exec("log #{url}/#{repo}/#{get_branch(branch)}/#{file} #{params}")
+        exec(repo, "log #{repository_path(repo)}/#{get_branch(branch)}/#{file} #{params}")
       end
 
       private
 
-      def url
-        return "svn://#{@url}" if @password
+      def repository_path(repo)
+        return "#{url}/#{repo}" unless assembla?
 
-        "svn+ssh://#{@username}@#{@url}"
+        url
+      end
+
+      def url
+        return @url if @password
+
+        u = URI(@url)
+        "svn+ssh://#{@username}@#{u.host}#{u.path}" unless assembla?
+        "svn+ssh://#{u.host}"
+      end
+
+      def assembla?
+        @assembla ||= URI(@url).host.include? 'assembla'
+      end
+
+      def repository_name
+        URI(@url)&.path.split('/').last
       end
 
       def get_branch(branch)
