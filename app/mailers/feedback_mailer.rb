3c22e41708f850d76938bc7fb45b3ef8f0b08372
AndriiMysko <usandrewmisko@gmail.com>
2022-01-26 11:47:45 +0200

    Fix emails

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class FeedbackMailer < ApplicationMailer
-  default from: 'from@example.com'
+  default from: Settings.mail_from
   layout 'mailer'
 
   def send_feedback
--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class InvitationMailer < ApplicationMailer
-  default from: 'from@example.com'
+  default from: Settings.mail_from
   layout 'mailer'
 
   def send_invitation
