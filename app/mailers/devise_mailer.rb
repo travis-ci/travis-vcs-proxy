53b50e9b1c42aeb2f9e21bc6af0175347999d2b8
Michał Rybiński <michal@travis-ci.com>
2022-03-18 14:45:04 +0100

    devise mailer assignement colon fix

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -5,7 +5,7 @@
         super
         @vcsproxy_link=Settings.web_url
         @travisci_link=Settings.travis_url
-        @support_mail=Settings.support_mail: 
-        @contact_mail=Settings.contact_mail: 
+        @support_mail=Settings.support_mail
+        @contact_mail=Settings.contact_mail 
       end
    end
